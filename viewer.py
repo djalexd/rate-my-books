a5f2939b59f42ca9865a52fdb6875982181dab81
alexdobjanschi <alex.dobjanschi@gmail.com>
2018-09-16 21:56:48 +0200

    better rating

--- viewer.py
+++ viewer.py
@@ -2,6 +2,7 @@ import os
 import sys
 import argparse
 import json
+import math
 
 parser = argparse.ArgumentParser(description='')
 parser.add_argument('--ratings_path', help='Specified the local path to store/sync ratings with Dropbox file metadata. This is a JSON file')
@@ -11,6 +12,12 @@ ratings_path = args.ratings_path
 with open(args.ratings_path, 'r') as fd:
     rated_books = json.load(fd)
 
-    sorted_books = sorted(rated_books, key=lambda b: b['rating'], reverse=True)
-    for book in [b for b in sorted_books if b['rating'] > 0.0 and b['ratings_count'] > 100]:
-        print '%s - %s (from %s)' % (book['name'], book['rating'], book['ratings_count']) 
+    def adjust_rating(book_json): 
+        if 'ratings_count' in book_json and book_json['ratings_count'] > 0:
+            return 0.7 * book_json['rating'] + 0.3 * math.log(book_json['ratings_count'], 10)
+        else:
+            return book_json['rating']
+    
+    sorted_books = sorted(rated_books, key=adjust_rating, reverse=True)
+    for index, book in enumerate([b for b in sorted_books if b['rating'] > 0.0]):
+        print '%2d. %s - %.2f (from %s)' % (index, book['name'], adjust_rating(book), book['ratings_count']) 
